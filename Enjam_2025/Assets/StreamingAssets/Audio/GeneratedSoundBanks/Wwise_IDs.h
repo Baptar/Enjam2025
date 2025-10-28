735d5c4570fa7c892e53de17fedb0cd0a6183da9
Heloxz <mlamacchia2408@gmail.com>
2025-10-28 16:01:20 +0100

    Work Unit + SoundBank

---
+++ Enjam_2025/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_DOOR = 2547633870U;
+        static const AkUniqueID PLAY_FLIPROOM = 2586345710U;
+        static const AkUniqueID PLAY_FS_PLAYER = 1413020905U;
+        static const AkUniqueID PLAY_INTERACT = 3621663528U;
+        static const AkUniqueID PLAY_INTERACTPICTURE = 2755497438U;
+        static const AkUniqueID PLAY_RADIO = 2953849999U;
+        static const AkUniqueID PLAY_SFX_DOORKNOCK = 572923566U;
+        static const AkUniqueID PLAY_SFX_LIGHT = 3401097362U;
+        static const AkUniqueID STOP_FS_PLAYER = 173549051U;
+        static const AkUniqueID TEST_EVENT = 4258653052U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTERBANK = 1542108333U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
